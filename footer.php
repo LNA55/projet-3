b07e2801ddaf21b329eb700953cb635dcd710d69
LNA55 <elena.hagege@gmail.com>
2014-06-26 10:02:36 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,104 @@
+<footer>
+
+	<section class="lanewsletter">
+  		<div class="text"> 
+  			<h6 id="ancre2">Qui nous sommes</h6><br>
+				<div class="row">
+			  		<div class="col-md-10 col-md-offset-1">
+			  			<p class="white">Vous souhaitez rejoindre la communauté, être averti de la date du lancement et recevoir nos offres promotionnelles? </p>
+  					</div>
+  				</div>
+  		<div class="action">
+			<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+				<form action="http://msieconnect.us7.list-manage.com/subscribe/post?u=eb159da88ff74c8a19b96e759&amp;id=6bae9f036a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Entrez ici votre adresse email" required>
+				    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <!-- <div style="position: absolute; left: -5000px;"> --> <div><input type="text" name="b_eb159da88ff74c8a19b96e759_6bae9f036a" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" value="Envoyer" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-default"></div>
+				</form>
+				</div>
+				<!--End mc_embed_signup-->
+  		</div>
+  		<div class="descendre">
+  			<div class="row">
+			  <div class="col-md-2 col-md-offset-5">
+				<a href="#ancre3" class="btn btn-circle"> 
+			  		<i class="fa fa-angle-double-down animated">
+				  	</i>
+				</a>
+			  </div>
+			</div>
+  		</div>
+	</section>
+
+	<section>
+		<div class="row" id="ancre3">
+		  
+		  <div class="col-md-3 col-md-offset-1">
+		  		<h4>L'essentiel</h4>
+		  		Ici le résumé de ce qu'est ce site et à qui il s'adresse. Ici le résumé de ce qu'est ce site et à qui il s'adresse.  
+		  </div>
+		 
+		  <div class="col-md-3 col-md-offset-0">
+		  		<h4>Liens utiles</h4>
+			  	<ol>
+			  		<li><a href ="../recettes.php">>> Recettes d'apéritifs</a></li>
+			  		<li><a href ="../recettes.php">>> Recettes de plats</a></li>
+			  		<li><a href ="../product.php">>> Produits</a></li>
+			  		<li><a href ="../infos.php">>> Qui sommes-nous</a></li>
+			  	</ol>
+		  </div>
+
+		  <div class="col-md-3 col-md-offset-0">
+		  		<h4>Actu</h4>
+		  		<a href="https://twitter.com/missboutargue" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @MissBoutargue</a></br>
+		  		<div class="fb-like" data-href="https://www.facebook.com/lamaisondelaboutargue" data-width="1000px" data-layout="standard" data-action="like" data-show-faces="true" data-share="false"></div>
+		  </div>
+
+		</div>
+	</section>	
+
+	<section> 
+		<div class="row" id="laderniere">
+		  	<div class="col-md-7">
+		  		<p class="gray">La Maison de la Boutargue © Copyright 2014 @LNAcorp</p>
+		  	</div>
+			<div class="col-md-4 col-md-offset-1">
+				<nav>
+					<a href="http://www.shoette.com">Conditions générales</a> |
+					<a href="http://www.shoette.com">Mentions légales</a> |
+					<a href="http://www.shoette.com">Contact</a>
+				</nav> <!-- Pourquoi ce n'est pas sur une seule ligne? C'est quoi le pixel qui traine tout seul? -->
+			</div>
+		</div>
+	</section>
+
+</footer>
+
+		<!-- Pour appeler les librairies Jquery -->	
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+	        <script src="<?php bloginfo('template_directory'); ?>/js/vendor/bootstrap.min.js"></script>
+	        <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	   		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+	        <script src="js/vendor/bootstrap.min.js"></script>
+	        <script src="js/main.js"></script>
+
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+	        <script>
+	            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	            e.src='//www.google-analytics.com/analytics.js';
+	            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	            ga('create','UA-XXXXX-X');ga('send','pageview');
+	        </script>
+
+    </body>
+</html>
+
